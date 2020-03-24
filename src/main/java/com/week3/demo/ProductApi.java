61e292706ea4f831a23b4e01dc9e7ac4edee63de
areka <arekadnik@gmail.com>
2020-03-24 19:08:20 +0100

    REst APi

--- src/main/java/com/week3/demo/ProductApi.java
+++ src/main/java/com/week3/demo/ProductApi.java
@@ -7,21 +7,53 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/products")
 public class ProductApi {
 
-//    @RequestMapping(method = RequestMethod.GET)  ---> czasami mozna uzywac
+    /*
+    Poprzez parametr
+    @RequestMapping(method = RequestMethod.GET)  ---> czasami mozna uzywac
     @GetMapping
-    public String getProducts(){
-        return "Hello word with GET";
+    public String getProducts(@RequestParam  String name, @RequestParam(required = false,defaultValue = "") String surname){
+        return "Hello " + name + " " + surname;
     }
+
+
+    Poprzez PATH
+    @GetMapping("/{name}")
+    public String getProducts(@PathVariable  String name){
+        return "Hello " + name ;
+    }
+
+    Poprzez HEADER
+    @GetMapping
+    public String getProducts(@RequestHeader  String name){
+        return "Hello " + name ;
+    }
+
+    //Body
+    @GetMapping
+    public String getProducts(@RequestBody  String name){
+        return "Hello " + name ;
+    }
+
+    @GetMapping
+    public String getProducts(@RequestParam String name, @RequestHeader(required = false, defaultValue = "") String surname) {
+        return "Hello " + name + " " + surname;
+    }
+*/
+
+
+
     @PostMapping
-    public String addProduct(){
+    public String addProduct() {
         return "Hello word with POST";
     }
+
     @PutMapping
-    public String modifyProducts(){
+    public String modifyProducts() {
         return "Hello word with Put";
     }
+
     @DeleteMapping
-    public String removeProducts(){
+    public String removeProducts() {
         return "Hello word with Delete";
     }
 
